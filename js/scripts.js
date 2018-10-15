7a5fab2f7bf8ff3266a811602675465ac80dbdb0
Jared Reando and Phil Mass <phil@>
2018-10-15 11:47:07 -0700

    Add jQuery tags and functions.

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  $("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+
+  $("p").click(function() {
+    alert("This is a paragraph");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+  $("div").click(function() {
+    alert("This is div text.");
+  });  
+});
